738f9f6f2adc7bda1276d8cf4215c90530cf7cc7
Natalia Betancourt <nataliabetancourt@Natalias-MacBook-Air.local>
2025-03-31 11:55:08 -0500

    Added Google Tag Manager

--- app/layout.js
+++ app/layout.js
@@ -59,10 +59,35 @@ export default function Layout({ children }) {
             `,
           }}
         />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5RM5MMCM');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body
         className={`${montserrat.className} text-grey-400 mx-auto text-sm md:text-base font-normal relative`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5RM5MMCM"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
+        {/* End Google Tag Manager (noscript) */}
+
         <Navbar />
         <main>{children}</main>
         <Footer />
